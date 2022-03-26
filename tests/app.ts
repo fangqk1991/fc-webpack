37e69993c820a789abc0d03f6419482f6494cbb6
fangqk1991 <me@fangqk.com>
2022-03-26 20:10:27 +0800

    (tests): 测试 element-ui icon 样式

--- tests/app.ts
+++ tests/app.ts
@@ -1,5 +1,6 @@
 import './element-variables.scss'
-import 'element-ui/lib/theme-chalk/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/icon.css';
 import { AppView } from './AppView'
 
 new AppView({
